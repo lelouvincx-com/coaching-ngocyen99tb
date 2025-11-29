d7c47e2d3663d9acdb0be39b0f52d57174ae0655
lelouvincx <dinhminhchinh3357@gmail.com>
2025-11-29 17:12:34 +0700

    Update editUrl in docusaurus.config.ts to point to the correct repository

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lelouvincx-com/coaching-ngocyen99tb/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
